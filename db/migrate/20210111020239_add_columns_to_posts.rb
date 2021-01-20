dfb52c29b4f466dbae1697a94320836c9629a05a
StevenDRiggs <sfriggs21@gmail.com>
2021-01-20 16:50:34 -0700

    changed db from sqlite3 to postgresql

--- db/migrate/20210111020239_add_columns_to_posts.rb
+++ db/migrate/20210111020239_add_columns_to_posts.rb
@@ -1,6 +1,6 @@
 class AddColumnsToPosts < ActiveRecord::Migration[6.0]
   def change
     add_column :posts, :title, :string
-    add_colum :posts, :content, :text
+    add_column :posts, :content, :text
   end
 end
