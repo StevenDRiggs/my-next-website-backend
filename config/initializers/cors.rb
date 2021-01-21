adba0187a3c1459580839fce1534841263d6dc62
StevenDRiggs <sfriggs21@gmail.com>
2021-01-20 20:27:19 -0700

    updated cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'https://protected-cliffs-01462.herokuapp.com'
 
     resource '*',
       headers: :any,
