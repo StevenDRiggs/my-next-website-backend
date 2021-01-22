ae15cf1376f0a5fad8928695d5ef3edf12ca9cc6
StevenDRiggs <sfriggs21@gmail.com>
2021-01-22 13:50:04 -0700

    added smtp settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,6 @@
+require_relative '../BASE_DOMAIN.rb'
+
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -54,6 +57,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "backend_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: BASE_DOMAIN }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
