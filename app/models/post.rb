758ee3d772f6e487cccb0354eb1c439a6d7b926d
StevenDRiggs <sfriggs21@gmail.com>
2021-02-08 02:53:28 -0700

    added serialization to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,22 @@
 class Post < ApplicationRecord
+  include ActiveModel::Serializers::JSON
+
+  attr_reader :slug
+
   has_many :comments
 
   def slug
     self.title.split(/\s+/).map{|word| word.downcase}.join('-')
+  end
+
+  def attributes
+    {
+      id: nil,
+      title: nil,
+      content: nil,
+      created_at: nil,
+      updated_at: nil,
+      slug: nil
+    }
   end
 end
